94adb48838c60fc0291202b6f06eafec30c78789
yerram shruthi <yerramshruthi4646@gmail.com>
2026-02-22 20:56:35 +0530

    first commit

---
+++ binary search.py
@@ -0,0 +1,18 @@
+nums = list(map(int, input("Enter sorted numbers: ").split()))
+target = int(input("Enter target: "))
+
+left = 0
+right = len(nums) - 1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] == target:
+        print("Index:", mid)
+        break
+    elif nums[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+else:
+    print(-1)
